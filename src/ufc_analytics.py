3e0c05752bd5f46cd542db524bed00b3a64367be
calvomorrison <77497606+calvomorrison@users.noreply.github.com>
2025-10-01 15:24:41 +0200

    chore: messing around with it pt. II

--- src/ufc_analytics.py
+++ src/ufc_analytics.py
@@ -38,11 +38,18 @@ print(fights_df.columns)
 
 # TRANSFORM
 
+
+# Merge
+
 fights_full_df = pd.merge(fights_df, events_df, "left", "Event_Id")
+
+# Calculations
 
 fighters_df["Total"] = fighters_df["W"] + fighters_df["D"] + fighters_df["L"]
 
 print(fighters_df[["Full Name", "Total"]])
+
+# Sorting and filtering
 
 fighters_df.sort_values(["Total", "W"], ascending=False)
 
@@ -56,5 +63,39 @@ print(w_belt_df.sort_values(["Total", "W"], ascending=False))
 w_belt_southpaw_df = w_belt_df[w_belt_df["Stance"] == "Southpaw"]
 print(w_belt_southpaw_df.sort_values(["Total", "W"], ascending=False))
 
+def calc_total(W, L, D):
+    return (W + L + D)
 
+#Aggretation Examples
 
+locations_df = events_df.groupby("Location")["Location"].count().reset_index(name="Count")
+
+print(locations_df)
+
+locations_df = locations_df.sort_values("Count", ascending=False)
+
+print(locations_df.head())
+
+# Funciones
+
+def clasificar_fighter(wins, losses):
+    if wins > 20 and losses < 5:
+        return "Elite"
+    elif 10 <= wins <= 20:
+        return "Promesa"
+    else:
+        return "En formación"
+    
+def calc_ratio(row):
+    total = row["W"] + row["L"]
+    if total >= 1:
+        return row["W"] / total
+    else:
+        return "Sin Datos"
+
+# Crear nueva columna
+fighters_df["WinRatio"] = fighters_df.apply(calc_ratio, axis=1)
+
+# Filtrar los que tienen WinRatio > 0.75
+fighters_df = fighters_df[fighters_df["WinRatio"] != "Sin Datos"]  # descartar los string
+fighters_df = fighters_df[fighters_df["WinRatio"] > 0.75]
